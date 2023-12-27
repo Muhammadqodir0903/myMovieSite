0f4f8be720e5cb98fbdaae47735fb7943e9b4a28
Muhammadqodir0903 <madinaziemova@gmail.com>
2023-12-28 00:25:26 +0500

    update movieSite

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-let partMovies = movies.slice(50, 101)
+let partMovies = movies.slice(50, 149)
 let elMoveList = document.querySelector('.movies__list')
+let elSelCat = document.querySelector('.sel__category')
 
 
-partMovies.forEach((item) => {
-    console.log(item);
-    let newLi = document.createElement('li')
-    newLi.innerHTML = `
+fnRender(partMovies)
+
+function fnRender(data) {
+    elMoveList.innerHTML = ''
+    data.forEach((item) => {
+        let newLi = document.createElement('li')
+        newLi.innerHTML = `
             <div class="card" style="width: 18rem;">
             <img src="https://i.ytimg.com/vi/${item.ytid}/hqdefault.jpg?" class="card-img-top" alt="...">
             <div class="card-body">
@@ -17,5 +21,43 @@ partMovies.forEach((item) => {
             </div>
         </div>
     `
-    elMoveList.appendChild(newLi)
+        elMoveList.appendChild(newLi)
+    })
+}
+
+function fnYear(value) {
+    if (value == 'new') {
+        fnRender(partMovies.sort((a, b) => b.movie_year - a.movie_year));
+    } else {
+        fnRender(partMovies.sort((a, b) => a.movie_year - b.movie_year));
+
+    }
+}
+
+function fnRanting(value) {
+    if (value == 'max') {
+        fnRender(partMovies.sort((a, b) => b.imdb_rating - a.imdb_rating));
+    } else {
+        fnRender(partMovies.sort((a, b) => a.imdb_rating - b.imdb_rating));
+
+    }
+}
+
+let arrCategory = []
+partMovies.forEach((item) => {
+   if(!arrCategory.includes(item.Categories)){
+    arrCategory.push(item.Categories)
+   }
 })
+
+arrCategory.forEach((item) => {
+   let newOption = document.createElement('option')
+   newOption.textContent = item
+   elSelCat.appendChild(newOption) 
+})
+
+function fnCategory(value) {
+    fnRender(partMovies.filter((item)=> item.Categories == value));
+}
+
+
