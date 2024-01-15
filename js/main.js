9da0cd17868052a45bc44c5fbcc9ec3d344a7194
Muhammadqodir0903 <madinaziemova@gmail.com>
2024-01-16 02:53:12 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 let partMovies = movies.slice(0, 100)
 let elMoveList = document.querySelector('.movies__list')
 let elSelCat = document.querySelector('.sel__category')
+let elOfcList = document.querySelector('.offcanvas-body__list')
+
+let saveLocData = []
 
 
 fnRender(partMovies)
+console.log(saveLocData.includes('jwD04NsnLLg'));
 
 function fnRender(data) {
+
     elMoveList.innerHTML = ''
     data.forEach((item) => {
         let newLi = document.createElement('li')
@@ -17,7 +22,10 @@ function fnRender(data) {
             <p class="card-text">${item.Categories}</p>
             <p class="card-text">${item.movie_year}-yil</p>
             <p class="card-text">${item.imdb_rating}⭐️</p>
-            <a href="https://www.youtube.com/watch?v=${item.ytid}" class="btn btn-warning" target="_blank">Watch Movie</a>
+        <div class = "d-flex justify-content-between"> 
+           <a href="https://www.youtube.com/watch?v=${item.ytid}" class="btn btn-warning" target="_blank">Watch Movie</a>
+        <i onclick=" fnFavourite('${item.ytid}')" class="${JSON.parse(window.localStorage.getItem('saveData'))?.find(k=> k.ytid == item.ytid ) ? 'bi bi-bookmark-heart-fill' : 'bi bi-bookmark-heart'}"></i>
+        </div>
             </div>
         </div>
     `
@@ -45,41 +53,75 @@ function fnRanting(value) {
 
 let arrCategory = []
 partMovies.forEach((item) => {
-   if(!arrCategory.includes(item.Categories)){
-    arrCategory.push(item.Categories)
-   }
+    if (!arrCategory.includes(item.Categories)) {
+        arrCategory.push(item.Categories)
+    }
 })
 
 arrCategory.forEach((item) => {
-   let newOption = document.createElement('option')
-   newOption.textContent = item
-   elSelCat.appendChild(newOption) 
+    let newOption = document.createElement('option')
+    newOption.textContent = item
+    elSelCat.appendChild(newOption)
 })
 
 function fnCategory(value) {
-    fnRender(partMovies.filter((item)=> item.Categories == value));
+    fnRender(partMovies.filter((item) => item.Categories == value));
 }
-function fnSearch(event){
+function fnSearch(event) {
     event.preventDefault()
     let val = event.target.search.value
-    fnRender(partMovies.filter((item)=> item.Title.toString().toLowerCase().includes(val.toLowerCase())));
+    fnRender(partMovies.filter((item) => item.Title.toString().toLowerCase().includes(val.toLowerCase())));
 }
 
-function fnReset(value){
+function fnReset(value) {
     if (value == '')
-    fnRender(partMovies)
+        fnRender(partMovies)
 }
-function fnPagenation(count){
-    fnRender(partMovies.slice((count-1) *10 ,count *10));
+function fnPagenation(count) {
+    fnRender(partMovies.slice((count - 1) * 10, count * 10));
 }
 let elPagenation = document.querySelector('.pagenation')
 for (let i = 0; i < partMovies.length / 10; i++) {
     console.log(i);
     let newButton = document.createElement('button')
-    newButton.textContent = i+1
+    newButton.textContent = i + 1
     newButton.classList = 'btn btn-warning ms-3'
-    newButton.setAttribute('onclick', `fnPagenation(${i+1})` )
+    newButton.setAttribute('onclick', `fnPagenation(${i + 1})`)
     elPagenation.appendChild(newButton)
-    
-   }
 
+}
+
+let saveData = []
+if (window.localStorage.getItem('saveData')) {
+    saveData = JSON.parse(window.localStorage.getItem('saveData'))
+}
+function fnFavourite(id) {
+    console.log(partMovies.find(k=> k == id));
+    if (window.localStorage.getItem('saveData')) {
+        saveData = JSON.parse(window.localStorage.getItem('saveData'))
+    }
+    if (saveData.find(k=> k.ytid == id)) {
+        window.localStorage.setItem('saveData', JSON.stringify(saveData.filter(item => item.ytid != id)))
+    } else {
+        saveData.push(partMovies.find(k=> k.ytid == id))
+        window.localStorage.setItem('saveData', JSON.stringify(saveData))
+
+    }
+    console.log(partMovies.find(k=> k.ytid == id));
+    fnRender(partMovies)
+}
+
+function fnMapLoc(){
+    elOfcList.innerHTML = ''
+   let data = JSON.parse( window.localStorage.getItem('saveData'))
+    data.map((item)=>{
+        let newLi = document.createElement('li')
+        newLi.style.height = '40px'
+        newLi.innerHTML = `      <a href="https://www.youtube.com/watch?v=${item.ytid}"  target="_blank" class="d-flex justify-content-between h-100 w-100  border text-decoration: none">
+        <img class="h-100"src="https://i.ytimg.com/vi/${item.ytid}/hqdefault.jpg?" class="card-img-top" alt="...">
+    <h3 class="pe-4">${item.Title.toString().slice(0,15)}</h3>
+    </a>`
+    
+    elOfcList.appendChild(newLi)
+    })
+}
